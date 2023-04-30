8c89776e5325ceb2dd5264295e5800c071d7ccee
Siraj Mohammed Saleem <sirajms5@yahoo.com>
2023-04-30 11:10:16 -0400

    editing and customization

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :friends
   get 'home/about'
-  root 'home#index'
+  root 'friends#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
